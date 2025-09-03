2499ab20bd243ab682b3dd7a2cd1b4391dc7c5a2
Talemul Islam <talemul@mislbd.com>
2025-09-03 13:34:06 +0600

    add vendor publish command

--- src/Console/InstallCommand.php
+++ src/Console/InstallCommand.php
@@ -37,12 +37,6 @@ class InstallCommand extends Command
         $this->initDatabase();
 
         $this->initAdminDirectory();
-
-        //        \Illuminate\Support\Facades\Artisan::call('vendor:publish', [
-        //            '--provider' => 'SuperAdmin\Admin\AdminServiceProvider',
-        //            '--force' => true,
-        //        ]);
-        //
         \Illuminate\Support\Facades\Artisan::call('vendor:publish', [
             '--tag' => 'super-admin-assets',
             '--force' => true,
